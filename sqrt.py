55ce8f5e7807f30ad8a1b3054b7a3b6e2cec4c2d
cis12104 <97993004+cis12104@users.noreply.github.com>
2022-01-25 00:28:37 -0600

    adding sum of odd numbers square root functions

---
+++ sqrt.py
@@ -0,0 +1,66 @@
+input_prompt = int(input("Enter a number: "))
+
+### Sum of odd numbers implementations of square root
+
+'''
+perfect squares are the sum of odd numbers
+1  = 1
+4  = 1+3
+9  = 1+3+5
+16 = 1+3+5+7
+25 = 1+3+5+7+0
+
+sqrt(16) = 1+3+5+7 <- we added 4 numbers to get to 16 -> sqrt(16) = 4
+sqrt(25) = 1+3+5+7+9 <- we added 5 numbers -> sqrt(25) = 5
+sqrt(21) = 1+3+5+7+9... 25 is over 21
+         = 1+3+5+7+5 => added 4 numbers together and remainder is 5/9 => sqrt(21) ~= 4+5/9
+sqrt(18) = 1+3+5+7+9... 25 is over 18
+         = 1+3+5+7+2 => 18 is over 16 by 2. so remainder is 2/9 => sqrt(18) ~= 4+2/9
+
+         implement using for/while?
+'''
+
+''' generating odd numbers
+index = 0; odd = 1;
+index = 1 ; odd = 3;
+index = 2; odd = 5;
+'''
+
+def sqrt1(inputnum):
+    print('using conditional-while')
+    acc = 0
+    index = 0
+    while (acc < inputnum):
+        odd = 2 * index + 1
+        acc += odd
+        index += 1
+    remainder = inputnum - (acc - odd) # for 18, 18-(25-9) = 18-16 = 2
+    return (index-1 + remainder/odd)
+
+def sqrt2(inputnum):
+    print('using while with break')
+    acc = 0
+    index = 0
+    while True:
+        odd = 2 * index + 1
+        acc += odd
+        if (acc > inputnum):
+            break
+        index += 1
+    remainder = inputnum - (acc - odd) # for 18, 18-(25-9) = 18-16 = 2
+    return (index + remainder/odd)
+
+
+def sqrt3(inputnum):
+    print('using for loop')
+    acc = 0
+    for index in range(1000):
+        odd = 2 * index + 1
+        acc += odd
+        #print(index, odd, acc)
+        if (acc > inputnum):
+            break
+    remainder = inputnum - (acc - odd) # for 18, 18-(25-9) = 18-16 = 2
+    return (index + remainder/odd) # for 18, 4+2/9'''
+
+#print(sqrt3(input_prompt))
